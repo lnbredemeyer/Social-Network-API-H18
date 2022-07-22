699f755cee05a092d8592b36a0861312ce0eb7cb
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-07-22 11:26:47 -0500

    added more to user route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -15,5 +15,7 @@ router.route('/').get(getUsers).post(createUser);
 // api users -- userID
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
+// api users -- userID friends
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
